579e98f3a73dc176730397a731ed7c3abb6d5e8f
moad eladib <144381364+Meladib@users.noreply.github.com>
2026-04-25 15:35:12 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=4 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python train.py \
-    --data-root /data/pos+mot/Datadir/ \
-    --exp-name rfmotip_dancetrack \
-    --config-path ./configs/rf_detr_motip_dancetrack.yaml
